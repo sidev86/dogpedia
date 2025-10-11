757e6cd6715e8290b83be2a3a3709d08ed843e58
Samir Ibrahim <samir.ibrahim@outlook.it>
2025-10-11 09:08:05 +0200

    Little changes on 'About' section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,6 +12,12 @@ function About() {
         intuitive interface.
       </p>
 
+      <p className="w-1/2 mx-auto mb-6">
+        Ready for a challenge? Dive into the Dog Quiz Game and see how many
+        breeds you can recognize! The faster you guess, the higher your score.
+        Let the fun begin! 🐶🐾
+      </p>
+
       <h2 className="text-2xl font-bold text-[#004f3b] mb-2">Tools Used</h2>
       <ul className="mb-6">
         <li>
@@ -28,11 +34,7 @@ function About() {
           <span className="font-bold">Vite:</span> Build tool
         </li>
       </ul>
-      <p className="w-1/2 mx-auto mb-6">
-        And if you wanna test your knowledge of dog breeds and have fun... try
-        the dog quiz game! Find out how much you know with our dog quiz in the
-        dedicated section! 🐶🐾
-      </p>
+
       <p>
         <span className="font-bold">Created by:</span> Samir Ibrahim
       </p>
